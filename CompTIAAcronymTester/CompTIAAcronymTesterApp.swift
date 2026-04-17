32e08aa917ee510f35e5e04d272bc8e31ac4b82f
Stuart Russell <stuartrussell@mac.com>
2026-04-16 23:57:28 -0700

    Initial Commit

---
+++ CompTIAAcronymTester/CompTIAAcronymTesterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompTIAAcronymTesterApp.swift
+//  CompTIAAcronymTester
+//
+//  Created by sbr on 4/16/26.
+//
+
+import SwiftUI
+
+@main
+struct CompTIAAcronymTesterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
